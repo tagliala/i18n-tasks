8f63e9abcfe7f11c2423dfb815ce4619f07ea8a0
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-06-27 12:03:21 +0200

    Fix RuboCop offenses
    - RSpec/FilePath, by changing description with uniformity with the spec
      in the same folder
    - RSpec/HooksBeforeExamples
    - RSpec/HookArgument
    - Style/FrozenStringLiteralComment
    - Style/RedundantRegexpArgument

--- spec/locale_tree/traversal_spec.rb
+++ spec/locale_tree/traversal_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe I18n::Tasks::Data::Tree::Traversal do
+RSpec.describe 'Tree traversal' do
   delegate :i18n_task, to: :TestCodebase
+
+  after do
+    TestCodebase.teardown
+  end
+
+  # --- setup ---
+  before do
+    TestCodebase.setup fixtures_contents
+  end
 
   describe '#grep_keys' do
     it 'returns Siblings' do
       expect(i18n_task.data['en'].grep_keys(/key/)).to be_a(I18n::Tasks::Data::Tree::Siblings)
     end
-  end
-
-  # --- setup ---
-  before(:each) do
-    TestCodebase.setup fixtures_contents
-  end
-
-  after do
-    TestCodebase.teardown
   end
 end
